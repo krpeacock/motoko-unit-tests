9cd04cd8a2450fa5bff1b7c0e1358d0d5531d095
Kyle Peacock <kylpeacock@gmail.com>
2022-02-05 11:42:12 -0800

    Initial commit.

---
+++ src/motoko_unit_tests_assets/src/index.js
@@ -0,0 +1,19 @@
+import { motoko_unit_tests } from "../../declarations/motoko_unit_tests";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await motoko_unit_tests.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
